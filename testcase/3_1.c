79379510d4c46bef9028d76b352df3320a15b685
zerqaq <1075843852@qq.com>
2020-10-03 20:37:06 +0800

    small fix

--- testcase/3_1.c
+++ testcase/3_1.c
@@ -1,15 +1,18 @@
-int test_function(){
+void unsafe_stack(){
     char var[100];
     int qwq[200];
     short owo[50];
     char test[10] = "12345678901234567890";
-    char *p = malloc(100);
-    memcpy(p, var, 1000);
     memset(qwq, 0, 100);
     memcpy(owo, qwq, 200);
     memcpy(qwq, qwq, 1000);
     memset(test, 0, 200);
-    return 1;
+}
+
+int unsafe_heap(){
+    char *p = malloc(100);
+    int var[100];
+    memcpy(p, var, 1000);
 }
 
 void unsafe_int(){
